6976fd054b6da889313d7f27d92714c04881d4b1
Justin Kruskie <justin@kruskie.net>
2024-10-02 11:24:53 -0400

    Create SecurityAuthenticator.java

---
+++ SecurityAuthenticator.java
@@ -0,0 +1,19 @@
+/**
+ * The SecurityAuthenticator class stores data about valid security 
+ * numbers for the Communications Authenticator programming challenge.
+ */
+
+// Create a public class named SecurityAuthenticator
+
+    // Create an integer array named valid that holds the following values:
+    // 5658845, 4520125, 7895122, 8777541, 8451277, 1302850, 
+    // 8080152, 4562555, 5552012, 5050552, 7825877, 1250255,
+    // 1005231, 6545231, 3852085, 7576651, 7881200, 4581002
+
+    /**
+     * The isValid method uses a sequential search to determine whether a 
+     * number appears in the array of valid numbers and returns true or false.
+     * 
+     * @param userInput The input number to validate.
+     * @return true if the input is valid, false otherwise.
+     */
