eb1e13cb2c0175df294d9869752582adcd899d47
Justin Kruskie <justin@kruskie.net>
2024-10-02 11:25:13 -0400

    Create CommunicationsPanel.java

---
+++ CommunicationsPanel.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CommunicationsPanel {
+    public static void main(String[] args) {
+        // Variables and scanner
+        Scanner scanner = new Scanner(System.in);
+        int securityNumber;
+
+        // Create a SecurityAuthenticator object named authenticator.
+
+        // Ask the user to "Enter your security authentication number: " and get the input.
+
+        // Use the isValid method from authenticator to determine if the number is valid.
+
+        // If valid, display: "That's a valid security authentication number."
+        // If invalid, display: "That's an INVALID security authentication number."
+
+        // Close the scanner and exit the application.
+    }
+}
